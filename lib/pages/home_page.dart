b9f627982bee9a28c1b0ad75167cde126f14c12d
Vincent <arareach@gmail.com>
2021-05-07 17:13:38 -0500

    Ejercicio del Profe

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -11,7 +11,86 @@ class HomePage extends StatelessWidget {
         ),
       ),
       body: Center(
-        child: Text("Hola Mundo"),
+        child: Column(children: [
+          Container(
+            width: 250,
+            height: 150,
+            alignment: Alignment.center,
+            margin: EdgeInsets.only(left: 5, top: 7, right: 15, bottom: 10),
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+            decoration: BoxDecoration(
+              color: Colors.amber,
+              borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(60),
+                  topRight: Radius.circular(40),
+                  bottomLeft: Radius.circular(30),
+                  bottomRight: Radius.circular(35)),
+            ),
+            child: Text("Lorem Ipsum is simply dummy text of the prin",
+                textAlign: TextAlign.center,
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
+                style: TextStyle(
+                    fontSize: 25,
+                    color: Colors.blue,
+                    fontWeight: FontWeight.w900,
+                    letterSpacing: 2,
+                    decoration: TextDecoration.none,
+                    height: 1.5)),
+          ),
+          Container(
+            width: 250,
+            height: 150,
+            alignment: Alignment.center,
+            margin: EdgeInsets.only(left: 5, top: 7, right: 15, bottom: 10),
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+            decoration: BoxDecoration(
+              color: Colors.amber,
+              borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(60),
+                  topRight: Radius.circular(40),
+                  bottomLeft: Radius.circular(30),
+                  bottomRight: Radius.circular(35)),
+            ),
+            child: Text("Lorem Ipsum is simply dummy text of the prin",
+                textAlign: TextAlign.center,
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
+                style: TextStyle(
+                    fontSize: 25,
+                    color: Colors.blue,
+                    fontWeight: FontWeight.w900,
+                    letterSpacing: 2,
+                    decoration: TextDecoration.none,
+                    height: 1.5)),
+          ),
+          Container(
+            width: 250,
+            height: 150,
+            alignment: Alignment.center,
+            margin: EdgeInsets.only(left: 5, top: 7, right: 15, bottom: 10),
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+            decoration: BoxDecoration(
+              color: Colors.amber,
+              borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(60),
+                  topRight: Radius.circular(40),
+                  bottomLeft: Radius.circular(30),
+                  bottomRight: Radius.circular(35)),
+            ),
+            child: Text("Lorem Ipsum is simply dummy text of the prin",
+                textAlign: TextAlign.center,
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
+                style: TextStyle(
+                    fontSize: 25,
+                    color: Colors.blue,
+                    fontWeight: FontWeight.w900,
+                    letterSpacing: 2,
+                    decoration: TextDecoration.none,
+                    height: 1.5)),
+          )
+        ]),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
